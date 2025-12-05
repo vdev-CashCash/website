35ae9b6845e791dae5ebd12b094919e4912fe3ac
Sample-02 <02sample.esp@gmail.com>
2025-12-05 09:59:10 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Accueil from "./Pages/Accueil";
 import Layout from "./Layout";
 import Error from "./Pages/Error";
+import Profil from "./Pages/Profil";
+import FichesEnCours from "./Pages/FichesEnCours";
+import RechercheFiches from "./Pages/RechercheFiches";
+import MesFiches from "./Pages/MesFiches";
+import Statistiques from "./Pages/Statistiques";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Accueil />} />
+          <Route path="/connexion" element={<Accueil />} />
+          <Route path="/mes_fiches" element={MesFiches} />
+          <Route path="/fiche_en_cours" element={FichesEnCours} />
+          <Route path="/" element={RechercheFiches} />
+          <Route path="/recherche_fiches" element={RechercheFiches} />
+          <Route path="/profil" element={Profil} />
+          <Route path="statistiques" element={Statistiques} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import logo from "../imgs/cashcashlogo.png";
 
 export default function Header() {
     return (
-        <header>
-            <Link to="/">Accueil</Link>
+        <header className="grid grid-cols-6 justify-center items-center border-b-2 border-b-red-500 w-screen">
+            <img src={logo} alt="CashCash" className="w-28" />
+            <Link to="/mes_fiches" className="justify-self-center border-2 border-slate-300 bg-slate-50 rounded-md p-2 hover:bg-white hover:border-slate-100 hover:text-slate-800"><button className="uppercase font-semibold">Mes fiches</button></Link>
+            <Link to="/recherche_fiches" className="justify-self-center border-2 border-slate-300 bg-slate-50 rounded-md p-2 hover:bg-white hover:border-slate-100 hover:text-slate-800"><button className="uppercase font-semibold">Rechercher une fiche</button></Link>
+            <Link to="/fiche_en_cours" className="justify-self-center border-2 border-slate-300 bg-slate-50 rounded-md p-2 hover:bg-white hover:border-slate-100 hover:text-slate-800"><button className="uppercase font-semibold">En cours</button></Link>
+            <Link to="/statistiques" className="justify-self-center border-2 border-slate-300 bg-slate-50 rounded-md p-2 hover:bg-white hover:border-slate-100 hover:text-slate-800"><button className="uppercase font-semibold">Statistiques</button></Link>
+            <Link to="/profil" className="justify-self-end pr-5"><button className="uppercase font-semibold">Profil</button></Link>
         </header>
     )
 }
