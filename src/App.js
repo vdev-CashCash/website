2996af6644e469f166820aaf400796e3ccd5739b
louchie5 <tichattibineloubna@gmail.com>
2025-12-05 09:02:14 +0100

    Page de connexion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Accueil from "./Pages/Accueil";
 import Layout from "./Layout";
 import Error from "./Pages/Error";
+import Connexion from "./Pages/Connexion";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Accueil />} />
           <Route path="*" element={<Error />} />
+          <Route path="/connexion" element={<Connexion />} />
         </Route>
       </Routes>
     </Router>
