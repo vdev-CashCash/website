089a079fe46b283346a9aa6d4e1e0e59b9e67c7a
Sample-02 <02sample.esp@gmail.com>
2026-02-09 16:41:29 +0100

    Connexion API Opérationnelle

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import './LoginSignup.css'
 import user_icon from '../Assets/login.png'
 import password_icon from '../Assets/password.png'
+import { useState } from 'react';
+
+
 
 const LoginSignup = () => {
+    const [useMatricule, setMatricule] = useState('');
+    const [usePassword, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await Login(useMatricule, usePassword);
+            window.location.href = '/#/mes_fiches';
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
         return (
             <div className= 'container'>
             <div className= 'header'>
@@ -12,18 +28,38 @@ const LoginSignup = () => {
             <div className='inputs'>
             <div className='input'>
             <img src={user_icon} alt="" />
-            <input type="text" />
+            <input type="text" placeholder="Matricule interne" required value={useMatricule} onChange={(e) => setMatricule(e.target.value)} />
             </div>
             <div className='input'>
             <img src={password_icon} alt="" />
-            <input type="password" />
+            <input type="password" placeholder="Mot de passe" required value={usePassword} onChange={(e) => setPassword(e.target.value)} />
             </div>
             </div>
             <div className="submit-container">
-            <div className="submit">Login</div>
+            <div className="submit" onClick={handleLogin}>Login</div>
             </div>
             </div>
         )
     }
+
+const Login = async (matricule, password) => {
+    const API_URL = 'https://elf-api.koyeb.app/elf-cashcash-api';
+    try {
+    const response = await fetch(`${API_URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ matricule, password })
+    });
+    const data = await response.json();
+
+    localStorage.setItem('token', data.token);
+
+    return data;
+    } catch (error) {
+        throw error;
+    }
+}
 
 export default LoginSignup
